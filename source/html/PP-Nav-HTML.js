da5721d06567bbf9bb60f3d8076de90195bbbf40
Steven Velozo <steven@velozo.com>
2026-02-18 15:17:11 -0800

    New build, better routing handling

--- source/html/PP-Nav-HTML.js
+++ source/html/PP-Nav-HTML.js
@@ -1,11 +1,11 @@
 module.exports = { HTML:/*html*/`
 
 <div>
-	<span data-nav="PP-ADB"><a href="#" onclick="_Pict.providers['PP-Router'].navigateTo('PP-ADB');"><b class="pp_nav_short">Ad</b><em class="pp_nav_long">AppData</em></a></span>
-	<span data-nav="PP-TB"><a href="#" onclick="_Pict.providers['PP-Router'].navigateTo('PP-TB');"><b class="pp_nav_short">Tp</b><em class="pp_nav_long">Templates</em></a></span>
-	<span data-nav="PP-VB"><a href="#" onclick="_Pict.providers['PP-Router'].navigateTo('PP-VB');"><b class="pp_nav_short">Vw</b><em class="pp_nav_long">Views</em></a></span>
-	<span data-nav="PP-PB"><a href="#" onclick="_Pict.providers['PP-Router'].navigateTo('PP-PB');"><b class="pp_nav_short">Pr</b><em class="pp_nav_long">Providers</em></a></span>
-	<span data-nav="PP-TO"><a href="#" onclick="_Pict.providers['PP-Router'].navigateTo('PP-TO');"><b class="pp_nav_short">Ov</b><em class="pp_nav_long">Overrides</em></a></span>
+	<span data-nav="PP-ADB"><a href="#" onclick="_Pict.providers['PP-Router'].navigateTo('PP-ADB'); return false;"><b class="pp_nav_short">Ad</b><em class="pp_nav_long">AppData</em></a></span>
+	<span data-nav="PP-TB"><a href="#" onclick="_Pict.providers['PP-Router'].navigateTo('PP-TB'); return false;"><b class="pp_nav_short">Tp</b><em class="pp_nav_long">Templates</em></a></span>
+	<span data-nav="PP-VB"><a href="#" onclick="_Pict.providers['PP-Router'].navigateTo('PP-VB'); return false;"><b class="pp_nav_short">Vw</b><em class="pp_nav_long">Views</em></a></span>
+	<span data-nav="PP-PB"><a href="#" onclick="_Pict.providers['PP-Router'].navigateTo('PP-PB'); return false;"><b class="pp_nav_short">Pr</b><em class="pp_nav_long">Providers</em></a></span>
+	<span data-nav="PP-TO"><a href="#" onclick="_Pict.providers['PP-Router'].navigateTo('PP-TO'); return false;"><b class="pp_nav_short">Ov</b><em class="pp_nav_long">Overrides</em></a></span>
 </div>
 
 `};
